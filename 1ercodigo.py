9cdcccb401c15b6de4eb224ca4aa710ab21f2036
HéctorMtz13 <hectorjmartinez1995h@gmail.com>
2025-02-04 09:00:09 -0600

    update 1ercodigo.py

--- 1ercodigo.py
+++ 1ercodigo.py
@@ -14,30 +14,41 @@ class figura:
  def calculopentagono(lado):
       perimetro = 5 * lado 
       area = (5 / 4) * (lado ** 2) * (1 / math.tan(math.pi / 5))
-      return perimetro , area 
-    
+      return perimetro , area
+ 
+ def calculoflotante(lado, numlados):
+     perimetro = lado * numlados 
+     apotema = lado/ (2* math.tan(math.pi/ numlados))
+     area = (perimetro * apotema)/ 2
+     return perimetro , area 
 
 print ("Cual figura deseas enconyrarle su area y su perimetro")
 print("1. Cuadrado")
 print("2. Triángulo")
 print("3. Pentágono")
-opcion = int(input("Elige 1 de las 3 opciones : "))
+print("4 figura regular")
+opcion = int(input("Elige 1 de las 4 opciones : "))
 
 if opcion == 1:
     lado = float(input("Ingresingrese el lado del cuadrado"))
     perimetro , area = figura.calculocuadrado(lado)
-    print(f"El perímetro del cuadrado es:  {perimetro}")
-    print(f"El área del cuadrado es: {area}")
+    print(f"El perímetro del cuadrado es : {perimetro}")
+    print(f"El área del cuadrado es : {area}")
 elif opcion == 2:
     lado = float(input("Ingresa el lado del triángulo: "))
     perimetro , area = figura.calculotriangulo(lado)
-    print(f"El perímetro del triángulo es: {perimetro}")
-    print(f"El área del triángulo es: {area}")
+    print(f"El perímetro del triángulo es : {perimetro}")
+    print(f"El área del triángulo es : {area}")
 elif opcion == 3:
     lado = float(input("Ingresa el lado del pentagono: "))
     perimetro , area = figura.calculopentagono(lado)
-    print(f"Elperimetro del pentagono es :{perimetro}")
+    print(f"Elperimetro del pentagono es : {perimetro}")
     print(f"el area del pentagono es : {area}")
-
+elif opcion == 4:
+    lado = float(input("ingresa el lado de tu figura"))
+    numlados = float(input("ingresa el numero de lados que tiene tu figura"))
+    perimetro , area = figura.calculoflotante(lado, numlados)
+    print(f"el perimetro de tu figura es : {perimetro}")
+    print(f"el area de tu figura es : {area}")
 else:
  print("Esa opcion no se encuentra en las opciones")
