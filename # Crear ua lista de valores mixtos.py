9e68068c28612e831365b20de89c1cf837cbb0c6
HéctorMtz13 <hectorjmartinez1995h@gmail.com>
2025-03-27 08:42:27 -0600

    create #crear ua lista de valores mixtos.py

---
+++ # Crear ua lista de valores mixtos.py
@@ -0,0 +1,26 @@
+# Crear ua lista de valores mixtos 
+# Crear una lista de 5 valores del mismo tipo con numpy
+# las listas anteriores convertirlas a tuplas y conjuntos 
+# crear un diccionario de 5 elementos con valore de distinto tipo
+
+from numpy import numpy
+import numpy as np
+
+Mi_lista = [1,"Karol",3,"hector",5]
+
+array = np.array([1,2,3,4,5])
+
+#Convertir a un conjunto
+conjunto = set(tuple(Mi_lista),tuple(array))
+print(conjunto)
+#Convertir a tupla
+Convertir_tupla = tuple(Mi_lista, array)
+print(Convertir_tupla)
+#Convertir a diccionario
+diccionario = {
+    "Nombre" = ["pepa","gorge","homero"], 
+    "edad" = ["-1","99", "120"],
+    "nacionalidad"=["peru", "venezuela", "mexico"]
+    }
+print(diccionario)
+
