d1325a94686074d768f1905f150d9542d0e20c91
HéctorMtz13 <hectorjmartinez1995h@gmail.com>
2025-05-21 18:34:40 -0600

    numpy.py

---
+++ Actividad 5-Numpy/numpy.py
@@ -0,0 +1,17 @@
+import numpy
+
+lista1 = ["hello", True, 1, "Karol", 3]
+#lista2 = np.array([1, 2, 3, 4, 5])
+
+tupla1 = tuple(lista1)
+conjunto1 = set(lista1)
+diccionario1 = {"Nombre":"Hector",
+                "Edad": 19,
+                "Casado": True,
+                "Ocupación": "student",
+                "Años laborando": 4,}
+
+print(lista1)
+print(tupla1)
+print(conjunto1)
+print(diccionario1)
