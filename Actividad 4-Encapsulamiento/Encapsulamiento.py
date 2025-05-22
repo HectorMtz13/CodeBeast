5d0b38507fb23c51c106d7b82d1ed7eea1ac9d8a
HéctorMtz13 <hectorjmartinez1995h@gmail.com>
2025-05-21 18:30:19 -0600

    create Encapsulamiento.py

---
+++ Actividad 4-Encapsulamiento/Encapsulamiento.py
@@ -0,0 +1,14 @@
+class CuentaDeBanco:
+    def __init__ (self, nombre, nip, saldo):
+        self.nombre = nombre
+        self.__nip = nip
+        self.__saldo = saldo
+    
+    def mostrarSaldo(self):
+        return self.__saldo
+
+#---------------------------------------Iniciating programing
+tarjeta = CuentaDeBanco("Karol Hernandez", 1234, 500)
+print(tarjeta.nombre)
+print(tarjeta.mostrarSaldo())
+#print(tarjeta.__nip)
